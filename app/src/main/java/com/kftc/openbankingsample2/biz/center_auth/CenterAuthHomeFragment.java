d915595dd9ceec24a03d5ae4cd8e772640e78000
gmldus0582 <gmldus0582@naver.com>
2021-07-19 18:19:59 +0900

    first

---
+++ app/src/main/java/com/kftc/openbankingsample2/biz/center_auth/CenterAuthHomeFragment.java
@@ -0,0 +1,78 @@
+package com.kftc.openbankingsample2.biz.center_auth;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kftc.openbankingsample2.R;
+import com.kftc.openbankingsample2.biz.center_auth.api.CenterAuthAPIFragment;
+import com.kftc.openbankingsample2.biz.center_auth.api.account_balance.CenterAuthAPIAccountBalanceFragment;
+import com.kftc.openbankingsample2.biz.center_auth.api.account_transaction.CenterAuthAPIAccountTransactionRequestFragment;
+import com.kftc.openbankingsample2.biz.center_auth.api.transfer_result.CenterAuthAPITransferResultFragment;
+import com.kftc.openbankingsample2.biz.center_auth.api.transfer_withdraw.CenterAuthAPITransferWithdrawFragment;
+import com.kftc.openbankingsample2.biz.center_auth.api.user_me.CenterAuthAPIUserMeRequestFragment;
+import com.kftc.openbankingsample2.biz.center_auth.auth.CenterAuthFragment;
+import com.kftc.openbankingsample2.biz.main.HomeFragment;
+
+/**
+ * 센터인증 메인화면
+ */
+public class CenterAuthHomeFragment extends AbstractCenterAuthMainFragment {
+
+    // context
+    private Context context;
+
+    // view
+    private View view;
+
+    // data
+    private Bundle args;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+        args = getArguments();
+        if (args == null) args = new Bundle();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_center_auth_home, container, false);
+        initView();
+        return view;
+    }
+
+    private void initView() {
+
+        // 계좌등록
+//        view.findViewById(R.id.btnAuthToken).setOnClickListener(v -> startFragment(CenterAuthFragment.class, args, R.string.fragment_id_center_auth));
+
+        // API 거래
+       // view.findViewById(R.id.btnAPICallMenu).setOnClickListener(v -> startFragment(CenterAuthAPIFragment.class, args, R.string.fragment_id_center_api_call));
+        view.findViewById(R.id.btnInqrUserInfoPage).setOnClickListener(v -> startFragment(CenterAuthAPIUserMeRequestFragment.class, args, R.string.fragment_id_api_call_userme));
+
+        // 잔액조회
+        view.findViewById(R.id.btnInqrBlncPage).setOnClickListener(v -> startFragment(CenterAuthAPIAccountBalanceFragment.class, args, R.string.fragment_id_api_call_balance));
+
+        // 거래내역조회
+        view.findViewById(R.id.btnInqrTranRecPage).setOnClickListener(v -> startFragment(CenterAuthAPIAccountTransactionRequestFragment.class, args, R.string.fragment_id_api_call_transaction));
+
+        //출금 이체
+        view.findViewById(R.id.btnTrnsWDPage).setOnClickListener(v -> startFragment(CenterAuthAPITransferWithdrawFragment.class, args, R.string.fragment_id_api_call_withdraw));
+
+        // 이체결과조회
+        view.findViewById(R.id.btnTransferResult).setOnClickListener(v -> startFragment(CenterAuthAPITransferResultFragment.class, args, R.string.fragment_id_api_call_transfer_result));
+    }
+
+    @Override
+    public void onBackPressed() {
+        startFragment(HomeFragment.class, args, R.string.fragment_id_home);
+    }
+}
